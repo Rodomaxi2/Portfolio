7ee9b8af6588b565a7a67ff5bd6e7b2e8f5a9733
Rodolfo Maximo <rodomaxi2@gmail.com>
2021-09-12 16:12:28 -0500

    Desing of navbar

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,42 +1,28 @@
+import { Box, Stack, Heading, Flex, Text } from "@chakra-ui/react";
 import Link from "next/link";
 
 const Navbar = () => (
-	<nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
-		<div className="container">
-			<Link href="/">
-				<a className="navbar-brand">Welcome to my portfolio</a>
-			</Link>
-			<button
-				className="navbar-toggler"
-				type="button"
-				data-bs-toggle="collapse"
-				data-bs-target="#navbarNav"
-				aria-controls="navbarNav"
-				aria-expanded="false"
-				aria-label="Toggle navigation"
-			>
-				<span className="navbar-toggler-icon" />
-			</button>
-			<div className="collapse navbar-collapse" id="navbarNav">
-				<ul className="navbar-nav ml-left">
-					<li className="nav-item">
-						<a className="nav-link active" aria-current="page" href="/">
-							About
-						</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="/">
-							Projects
-						</a>
-					</li>
-					<li className="nav-item">
-						<Link href="/contact">
-							<a className="nav-link">Contact</a>
-						</Link>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
+	<Box>
+		<Flex
+			direction="row"
+			w="100%"
+			h="48px"
+			pos="fixed"
+			top="0"
+			bg="#212529"
+			justifyContent="space-evenly"
+			alignItems="end"
+		>
+			<Text>
+				<Link color="white" href="/">
+					Welcome to my portfolio
+				</Link>
+			</Text>
+			<Stack direction="row" textColor="white" spacing="30">
+				<Text>Projects</Text>
+				<Text>Contact</Text>
+			</Stack>
+		</Flex>
+	</Box>
 );
 export default Navbar;
