50b431d8ed877700e6c1dcd19529f665aeaef745
Rodolfo <rodomaxi2@gmail.com>
2021-09-13 16:30:16 -0500

    ProjectCard created

--- Components/Projects.js
+++ Components/Projects.js
@@ -1,3 +1,7 @@
+import { Box, Center, Grid } from "@chakra-ui/react";
+import ProjectCard from "./projectCard";
+import dataProjects from "../data/info.json";
+
 const Projects = () => (
 	<div className="row">
 		<div className="col-md-12">
@@ -29,32 +33,6 @@ const Projects = () => (
 				<div className="card-body">
 					<h3 className="text-center">Drum Machine</h3>
 					<p>You can make music with your keyboard!!</p>
-				</div>
-			</div>
-			<div className="card">
-				<iframe
-					height="300"
-					scrolling="no"
-					title="Technical Documentation Page"
-					src="https://codepen.io/rodomaxi/embed/preview/ExXYdJX?default-tab=result&theme-id=dark"
-					frameBorder="no"
-					loading="lazy"
-					allowtransparency="true"
-					allowFullScreen="true"
-				>
-					See the Pen{" "}
-					<a href="https://codepen.io/rodomaxi/pen/ExXYdJX">
-						Technical Documentation Page
-					</a>{" "}
-					by Rodolfo Maximo (
-					<a href="https://codepen.io/rodomaxi" target="_blank" rel="noreferrer">
-						@rodomaxi
-					</a>
-					) on <a href="https://codepen.io">CodePen</a>.
-				</iframe>
-				<div className="card-body">
-					<h3 className="text-center">Documentation page</h3>
-					<p>You can learn a little bit of Go</p>
 				</div>
 			</div>
 		</div>
