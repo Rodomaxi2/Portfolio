98adee5288c1bc2d330cbb8e528665aba7f0f326
Rodolfo <rodomaxi2@gmail.com>
2021-09-13 19:23:43 -0500

    Added icons and links

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,12 +1,26 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { HStack, Box, Center, Link } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTelegramPlane, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => (
-	<HStack justifyContent="space-around">
-		{/* The default icon size is 1em (16px) */}
-		<Image src="/public/telegram-plane.svg" boxSize="60px" />
-		<Image src="/public/twitter.svg" boxSize="60px" />
-		<Image src="/public/envelope.svg" boxSize="60px" />
-	</HStack>
+	<Box h="70px">
+		<Center fontSize="22px">Find my social media</Center>
+		<HStack justifyContent="space-around">
+			{/* The default icon size is 1em (16px) */}
+			<Link href="https://t.me/FamousZev" target="_blank">
+				<FontAwesomeIcon icon={faTelegramPlane} size="5x" />
+			</Link>
+
+			<Link href="https://twitter.com/rodomaxim" target="_blank">
+				<FontAwesomeIcon icon={faTwitter} size="5x" />
+			</Link>
+
+			<Link href="https://t.me/FamousZev" target="_blank">
+				<FontAwesomeIcon icon={faEnvelope} size="5x" />
+			</Link>
+		</HStack>
+	</Box>
 );
 
 export default Footer;
